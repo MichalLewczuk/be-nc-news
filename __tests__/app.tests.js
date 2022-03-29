f2dae59323ffe0481a111fc17e8de83e7a7d8460
MichalLewczuk <lewczukmichael84@gmail.com>
2022-03-29 14:50:29 +0100

    add and test GET /api/users endpoint ncnews-19

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -148,3 +148,18 @@ describe("PATCH /api/articles/:article_id", () => {
     expect(body.msg).toEqual("Invalid request body");
   });
 });
+
+describe("GET /api/users", () => {
+  test("Returns 200 status code and array of all users", async () => {
+    const res = await request(app).get("/api/users").expect(200);
+    expect(res.body.users).toBeInstanceOf(Array);
+    expect(res.body.users.length).toBe(4);
+    res.body.users.forEach((user) => {
+      expect(user).toMatchObject({
+        username: expect.any(String),
+        name: expect.any(String),
+        avatar_url: expect.any(String),
+      });
+    });
+  });
+});
